f498b9fbe270f02f25d3c26e82aa3a03782b667e
tkilias <torsten@kilias.de>
2018-12-26 22:02:58 +0100

    fix key location

--- bin/run.sh
+++ bin/run.sh
@@ -40,8 +40,8 @@ if [[ -n "${PUBLIC_HOST_ADDR}" && -n "${PUBLIC_HOST_PORT}" ]]; then
     echo "====REMOTE FINGERPRINT===="
 
     echo "=> Setting up the reverse ssh tunnel"
-    echo "autossh -M 0 -NgR ${PROXY_PORT}:localhost:${DESTINATION_PORT} root@${PUBLIC_HOST_ADDR} -p ${PUBLIC_HOST_PORT} -i /root/private_key"
-    autossh -M 0 -NgR ${PROXY_PORT}:localhost:${DESTINATION_PORT} root@${PUBLIC_HOST_ADDR} -p ${PUBLIC_HOST_PORT} -i /root/private_key
+    echo "autossh -M 0 -NgR ${PROXY_PORT}:localhost:${DESTINATION_PORT} root@${PUBLIC_HOST_ADDR} -p ${PUBLIC_HOST_PORT} -i /root/private_key/key"
+    autossh -M 0 -NgR ${PROXY_PORT}:localhost:${DESTINATION_PORT} root@${PUBLIC_HOST_ADDR} -p ${PUBLIC_HOST_PORT} -i /root/private_key/key
 else
     echo "=> Running in public host mode"
     /usr/sbin/sshd -D
